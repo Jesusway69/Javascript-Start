756eb8d667e083a86a282ccb919146f8d7e39b86
JesusWay69 HP elitebook <jesusway69@gmail.com>
2025-02-10 23:45:02 +0100

    20250210 Realizados algunos ejercicios del módulo 10

--- Exercise-10.js
+++ Exercise-10.js
@@ -3,15 +3,56 @@ Clase 41 - Ejercicios: Manejo de errores
 Vídeo: https://youtu.be/1glVfFxj8a4?t=20392
 */
 
+function division(dividend, divisor) {
+    if (typeof dividend !== "number" || typeof divisor !== "number") {
+        throw new Error(`Los 2 argumentos deben ser numéricos`);
+
+    }
+    else if (divisor == 0) {
+        throw new DivisionByZero("No se puede dividir entre 0");
+
+    }
+    return dividend / divisor
+}
 // 1. Captura una excepción utilizando try-catch
+try {
+    console.log(division(hola, adios))
+} catch {
+    console.log("ERROR")
+}
 
 // 2. Captura una excepción utilizando try-catch y finally
+try {
+    console.log(division(15, 3))// Se ejecuta el primero si es correcto
+    console.log(division(hola))// Si este estuviera primero no se ejecutaría el segundo
+
+} catch (ex) {
+    console.log(`ERROR: ${ex.message}`)
+} finally {
+    console.log("Esto se ejecuta siempre")
+}
 
 // 3. Lanza una excepción genérica
+try {
+    console.log(division(hola, 2))
+} catch (ex) {
+    console.log(ex.name, ":", ex.message)
+}
+
 
 // 4. Crea una excepción personalizada
+class DivisionByZero extends Error {
+    constructor(errorMessage) {
+        super(errorMessage)
+    }
 
+}
 // 5. Lanza una excepción personalizada
+try {
+    console.log(division(50, 0))
+} catch (ex) {
+    console.log(ex.name, ":", ex.message)
+}
 
 // 6. Lanza varias excepciones según una lógica definida
 
