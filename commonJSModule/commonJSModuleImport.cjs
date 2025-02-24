a412e9321cadaad43216f9deca56e50ac07b592e
JesusWay69 HP elitebook <jesusway69@gmail.com>
2025-02-24 23:55:51 +0100

    20250224 Pruebas import-export commonJS y ecmascriptJS

---
+++ commonJSModule/commonJSModuleImport.cjs
@@ -0,0 +1,4 @@
+const sum  = require('./commonJSModuleExport.cjs')
+
+console.log(sum(7, 8))
+
--- ecmaScriptModule/osInfo.mjs
+++ ecmaScriptModule/osInfo.mjs
@@ -1,10 +1,20 @@
-import { hostname, platform, cpus, arch, machine, totalmem, freemem, release } from "node:os"
+import os from "node:os"
 
-console.log(hostname())
-console.log(platform())
-console.log(release())
-console.log(arch())
-console.log(machine())
-console.log(totalmem() / 1024 / 1024)
-console.log(freemem() / 1024 / 1024)
-console.log(cpus())
+console.log(os.hostname())
+console.log(os.platform())
+console.log(os.release())
+console.log(os.arch())
+console.log(os.machine())
+console.log("Memoria RAM total: ",(os.totalmem() / 1024 / 1024/1000).toFixed(), " Gb")
+console.log("Memoria RAM libre: ",(os.freemem() / 1024 / 1024/1000).toFixed(2), "Gb")
+//console.log(os.cpus())
+console.log(os.userInfo())
+console.log("Nº de procesadores lógicos: ", os.cpus().length)
+console.log("Nº de procesadores físicos: ", os.cpus().length/2)
+console.log("modelo de procesador: ", os.cpus()[0].model)
+console.log("Velocidad base del procesador: ", parseFloat(os.cpus()[0].speed/1000, 10).toFixed(1), " Ghz")
+
+for(let core of os.cpus()){
+    console.log(core.speed)
+    console.log(core.times.user)
+}
